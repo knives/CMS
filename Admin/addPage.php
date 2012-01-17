794471d8f83e654b1886bc0ea9a59ec938e5e1e0
knives <jerome13nguyen@laposte.net>
2012-01-17 19:22:22 +0100

    Admin/addPage.php

--- Admin/addPage.php
+++ Admin/addPage.php
@@ -1,4 +1,50 @@
 <?php
 include_once '../include/librairie.php';
+include_once '../classes/ListePages.php';
+include_once '../classes/Pages.php';
 VerifConnexion();
+$lp=new ListePages();
+$lp->GetListePages();
+print_a($lp,0,1);
 ?>
+<table cellpadding="0" cellspacing="0" style="width: 100%;">
+    <tr>
+        <td>Param&eacute;trage des Pages</td>
+    </tr>
+    <tr>
+        <td>
+            <table cellpadding="0" cellspacing="0" style="width: 100%;">
+                <thead>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    for($i=0;$i<$lp->nbPages;$i++){
+                        $page =new Pages();
+                        $page &=$lp->arrayPage[$i];
+                    ?>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+</table>
